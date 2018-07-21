43f41c8b07beebbaa140a23c8253273bf8b06e49
안단태 <48adt@naver.com>
2018-07-22 05:24:57 +0900

    Tutorial making...

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour {
+
+    private int phase;  // 0: XY평면 상 이동, 1: 시간축 상 이동, 2: 멈춘 적 맞추기
+
+	// Use this for initialization
+	void Start () {
+        phase = 0;
+	}
+	
+	void FixedUpdate () {
+		
+	}
+}
