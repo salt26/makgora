a5a3e925391ea43ca274d644cabeb201ba38e825
Ray <cbda@naver.com>
2018-08-04 19:43:45 +0900

    Scene Change

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -2,24 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class SceneChange : MonoBehaviour {
 
     public GameObject loadingPanel;
+    AsyncOperation asyncLoad = null;
+
+    public void QuitButton()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else 
+		Application.Quit();
+#endif
+    }
 
     public void LoadTutorial()
     {
         loadingPanel.SetActive(true);
         StartCoroutine(LoadTutorialScene());
-        //SceneManager.LoadScene("Tutorial");
     }
-            
+
+    public void LoadVagabondrHard()
+    {
+        loadingPanel.SetActive(true);
+        StartCoroutine(LoadVagabondHardScene());
+    }
+    
     IEnumerator LoadTutorialScene()
     {
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Tutorial");
-        while(!asyncLoad.isDone)
+        yield return new WaitForSeconds(1f);
+        asyncLoad = SceneManager.LoadSceneAsync("Tutorial");
+        while (!asyncLoad.isDone)
         {
-            yield return null;
+            yield return new WaitForSeconds(1f);
         }
+        yield return null;
+    }
+
+    IEnumerator LoadVagabondHardScene()
+    {
+        yield return new WaitForSeconds(1f);
+        asyncLoad = SceneManager.LoadSceneAsync("VagabondH");
+        while (!asyncLoad.isDone)
+        {
+            yield return new WaitForSeconds(1f);
+        }
+        yield return null;
     }
 }
