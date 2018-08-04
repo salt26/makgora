d2139f57e8e1f1e75051e9f44ca5739b8d6702fb
Ray <cbda@naver.com>
2018-08-04 18:48:13 +0900

    loading animation

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour {
+
+    public GameObject loadingPanel;
+
+    public void LoadTutorial()
+    {
+        loadingPanel.SetActive(true);
+        StartCoroutine(LoadTutorialScene());
+        //SceneManager.LoadScene("Tutorial");
+    }
+            
+    IEnumerator LoadTutorialScene()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Tutorial");
+        while(!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
