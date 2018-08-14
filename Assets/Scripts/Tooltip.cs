f0990ea50befc2dadb4ebca7fd51ea82c03fdf96
Ray <cbda@naver.com>
2018-08-14 20:57:32 +0900

    tooltips

--- Assets/Scripts/Tooltip.cs
+++ Assets/Scripts/Tooltip.cs
@@ -8,20 +8,22 @@ using UnityEngine.SceneManagement;
 public class Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
 
     public GameObject tooltipPanel;
-    public GameObject tooltipPanelClone;
-    public Text tooltipText;
+    private GameObject tooltipPanelClone;
     public Transform canvas;
     public string text;
     public int x, y;
+    public int width, height;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        tooltipText.text = text;
         tooltipPanelClone = Instantiate(tooltipPanel, new Vector3(x, y, 0), Quaternion.identity, canvas);
+        tooltipPanelClone.GetComponent<RectTransform>().sizeDelta = new Vector2(width,height);
+        tooltipPanelClone.GetComponentInChildren<Text>().text = text;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         Destroy(tooltipPanelClone);
+        tooltipPanelClone = null;
     }
 }
