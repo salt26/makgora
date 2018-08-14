b32b35aea60e98b2bdc5dc55b1cb102667dc7408
Ray <cbda@naver.com>
2018-08-14 18:10:55 +0900

    working on tooltip

--- Assets/Scripts/Tooltip.cs
+++ Assets/Scripts/Tooltip.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
 
+    public GameObject tooltipPanel;
+    public GameObject tooltipPanelClone;
     public Text tooltipText;
+    public Transform canvas;
 
-	public void ConcentraionTooltip()
+    public void OnPointerEnter(PointerEventData eventData)
     {
-        
+        tooltipText.text = "튜토리얼입니다.";
+        tooltipPanelClone = Instantiate(tooltipPanel, new Vector3(750, 514, 0), Quaternion.identity, canvas);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Destroy(tooltipPanelClone);
     }
 }
